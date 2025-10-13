84b0677ce6712753112c877998735feb61fe00d8
aschne-dev <a.schneberger+dev@gmail.com>
2025-10-13 22:44:09 +0200

    ✨ MAIN: TMDB API > Handle Errors

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,3 +1,17 @@
-export default function Search() {
-  return <div className="text-white text-3xl">Search Component</div>;
+export default function Search({ searchTerm, setSearchTerm }) {
+  // RENDER
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
