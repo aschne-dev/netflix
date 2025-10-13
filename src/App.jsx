d559d4375ed5a45bb6f3e99ed61bc53cbfe62853
aschne-dev <a.schneberger+dev@gmail.com>
2025-10-13 21:36:04 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without The Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+}
